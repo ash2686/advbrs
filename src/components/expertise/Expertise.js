60945b1e391c45018da48d53f2e637aad2bce87b
ash2686 <ash.centaur@gmail.com>
2021-12-09 15:29:40 +1300

    tweaked for mobile

--- src/components/expertise/Expertise.js
+++ src/components/expertise/Expertise.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Expertise.css'
 import Exprt from './Exprt'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import insurance from '../../assets/insurance.jpg'
 import banking from '../../assets/banking.jpg'
 import marraige from '../../assets/marraige.jpg'
@@ -10,53 +10,53 @@ import traffic from '../../assets/traffic.png'
 import business from '../../assets/business.png'
 
 const Expertise = () => {
-    return (
-        <div className='expertise-main'>
-          <div className="expertise-block1">
-         <Exprt 
-                img = {insurance}
-                 heading='Insurance Claims'
-                  para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
-                 
-          />
+        return (
+                <div className='expertise-main'>
+                        <div className="expertise-block1">
+                                <Exprt
+                                        img={insurance}
+                                        heading='Insurance Claims'
+                                        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
 
-<Exprt             
-                img = {banking}
-                 heading='Banking Claims'
-                  para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'                  
-          />
+                                />
 
-  <Exprt 
-                img = {marraige}
-                 heading='Marraige Disputes'
-                  para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
-          />
-        
-          </div>
+                                <Exprt
+                                        img={banking}
+                                        heading='Banking Claims'
+                                        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
+                                />
 
-          <div className="expertise-block2">
-          <Exprt 
-                img = {business}
-                 heading='Business Matters'
-                  para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
-          />
+                                <Exprt
+                                        img={marraige}
+                                        heading='Marraige Disputes'
+                                        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
+                                />
 
-           <Exprt 
-                img = {law}
-                 heading='General Law'
-                  para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
-          />
+                        </div>
 
-            <Exprt 
-                img = {traffic}
-                 heading='Traffic Offenses'
-                  para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
-          />
-         
-          </div>
-         
-        </div>
-    )
+                        <div className="expertise-block2">
+                                <Exprt
+                                        img={business}
+                                        heading='Business Matters'
+                                        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
+                                />
+
+                                <Exprt
+                                        img={law}
+                                        heading='General Law'
+                                        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
+                                />
+
+                                <Exprt
+                                        img={traffic}
+                                        heading='Traffic Offenses'
+                                        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, provident? Beatae soluta nisi unde modi?'
+                                />
+
+                        </div>
+
+                </div>
+        )
 }
 
 export default Expertise
