80839b9523ebd773c8255cb925ca87bac8a68dc0
ash2686 <ash.centaur@gmail.com>
2021-12-10 20:37:55 +1300

    added color to case

--- src/components/case/Case.js
+++ src/components/case/Case.js
@@ -8,10 +8,10 @@ const Case = () => {
                 <div className="case-line"></div>
                   <div className="case-text-block">
                       <h1>Case Results</h1>
-                       <p>Case results speak for themselves. I’ve helped my clients obtain millions of dollars in settlements and judgments.</p>
+                       <p style={{width:"100%"}}>Case results speak for themselves. I’ve helped my clients obtain millions of dollars in settlements and judgments.</p>
                        <p>If you’ve been injured or if you’ve suffered a loss, you need an experienced and successful attorney to fight for you and your loved ones.</p>
                        <p>High volume attorneys may promise you a quick settlement. But, they may not take the time to review your case as carefully as I will. You’ll get a quick settlement, but your case may be worth much more.</p>
-                       <p style={{width:"100%"}}>Don’t settle for less than you deserve.</p>
+                       <p style={{color: "red", fontWeight:"bolder"}}>Don’t settle for less than you deserve.</p>
                        <p style={{width:"100%"}}>Here are some recent case results & settlements:</p>
                          
                   </div>
